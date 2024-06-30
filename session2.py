6551ae04888d4c71dbc72495761da2a0994a57c2
msa-sadeghi <sadeghi.msai@gmail.com>
2024-06-30 20:55:22 +0330

    Create session2.py

---
+++ session2.py
@@ -0,0 +1,36 @@
+# x = 1
+# print(type(x))
+# y = 1.1
+# print(type(y))
+
+# a = int(input("enter a number: "))
+# b = 4
+
+# z = a + b
+# print(z)
+
+# x = int(input("enter a number: "))
+# print(x % 2 == 0)
+
+# score = 0
+# score = score + 1
+# print(score)
+
+# score += 1
+# print(score)
+
+# a = 4
+# b = 5
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+# برنامه ای بنویس که دو عدد از ورودی دریافت کند و آن ها را با هم مقایسه نماید
+
+x = int(input("enter a number"))
+y = int(input("enter a number"))
+
+print("sum is", x + y)
