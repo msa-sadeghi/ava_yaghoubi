0283b8e33e30e881bc84c8f5c2d6c57c8b78cd3c
msa-sadeghi <sadeghi.msai@gmail.com>
2024-07-21 20:58:07 +0330

    a

--- session4.py
+++ session4.py
@@ -7,12 +7,30 @@
 
 # print(f"{name} welcome to {course_name} class")
 
-name = input("enter a name: ")
-for x in name:
-    print(x, end=" ")
+# name = input("enter a name: ")
+# for x in name:
+#     print(x, end=" ")
 
 # print("a", "v", "a", sep="*", end=" ")  
 # print("benita") 
 
-numbers = "123456789"
+# numbers = "123456789"
 # calc the sum of digits in numbers with for loop
+# res = 0
+# for d in numbers:
+#     res += int(d) * 2
+# print(res)
+
+names = ["ramin", "artin", "sara","benita", "ava","roze"]
+for n in names:
+    print("hello", n)
+c = 0
+for n in names:
+    # if n[len(n)-1] == "n":
+    if n[-1] == "n":
+        c += 1
+print(c)
+# چند تا از اسم ها دو تا 
+# a
+# دارند
+        
