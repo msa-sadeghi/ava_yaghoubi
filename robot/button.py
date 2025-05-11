bdde2bd13318632451f24057876455bfd70df5ab
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-05-11 20:06:27 +0330

    ss

---
+++ robot/button.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Button:
+    def __init__(self, image, x,y):
+       
+        self.image = pygame.transform.scale_by(
+            pygame.image.load(image), 0.2)
+        self.rect = self.image.get_rect(
+            topleft=(x,y))
+
+
+    def update(self, screen):
+        screen.blit(self.image, self.rect)
+        click = False
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                click = True
+
+        return click
--- robot/level_editor.py
+++ robot/level_editor.py
@@ -1,21 +1,36 @@
 import pygame 
+from button import Button
 pygame.init()  # Initialize pygame
 import os
 WIDTH = 800
 HEIGHT = 600
-SIDE_MARGIN = 400
+SIDE_MARGIN = 600
 LOWER_MARGIN = 100
 FPS = 60
 
 tile_images =  []
 tiles = os.listdir("game_world/Tiles")  # Number of tiles in the folder
 for tile in tiles:
-    tile_images.append(tile)  # Load the images
-objects = os.listdir("game_world/Objects")  # Number of tiles in the folder
-for obj in objects:
-    tile_images.append(obj)  # Load the images
+    tile_images.append("game_world/Tiles/" +tile)  # Load the images
 
-print(tile_images)
+objects = os.listdir("game_world/Objects")  # Number of tiles in the folder
+object_images =  []
+for obj in objects:
+    object_images.append("game_world/Objects/"+obj)  # Load the images
+
+tile_btns = []
+tile_row =0
+tile_col = 0
+
+for tile_img in tile_images:
+    b = Button(tile_img, WIDTH + 20 + tile_col * 92, 20 + tile_row * 92)
+    tile_btns.append(b)
+    tile_col += 1
+    if tile_col == 6:
+        tile_row += 1
+        tile_col = 0
+
+
 
 screen = pygame.display.set_mode((WIDTH + SIDE_MARGIN, HEIGHT + LOWER_MARGIN))   
 pygame.display.set_caption("Level Editor")
@@ -29,6 +44,10 @@ while running:
     screen.fill("white")  
     pygame.draw.rect(screen, "lightblue", (WIDTH, 0, SIDE_MARGIN,HEIGHT + LOWER_MARGIN))  
     pygame.draw.rect(screen, "lightblue", (0, HEIGHT,WIDTH +  SIDE_MARGIN, HEIGHT)) 
+    for btn in tile_btns:
+        btn.update(screen)
+
+
     pygame.display.flip()  # Update the display
     clock.tick(FPS)  # Maintain the frame rate
 pygame.quit()  # Quit pygame
